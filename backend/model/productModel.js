471ec741828fcfcad128c2b05e365f6df7023490
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-07-01 19:31:31 +0530

    Cteates Product data model withing productSchema

---
+++ backend/model/productModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    price: {type: Number, required: true, default: 0},
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
