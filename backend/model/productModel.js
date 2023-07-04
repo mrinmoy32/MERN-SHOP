bf9762283c768640572eb2012530735d7ef576cb
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-07-04 22:12:23 +0530

    Changed key name to title

--- backend/model/productModel.js
+++ backend/model/productModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    title: {type: String, required: true},
     price: {type: Number, required: true, default: 0},
 });
 
